8058c91d3cb66e89b762a9782615cadbeb853329
DaviSenai <davi_bauer@estudante.sesisenai.org.br>
2022-12-07 00:22:35 -0300

    Modal + API

--- src/app/app-filter/app-filter.component.ts
+++ src/app/app-filter/app-filter.component.ts
@@ -6,29 +6,59 @@ import { AppService } from '../app.service';
   templateUrl: './app-filter.component.html',
   styleUrls: ['./app-filter.component.css']
 })
-export class AppFilterComponent {
+export class AppFilterComponent implements OnInit {
 
   appService: AppService;
 
-  apihref : string = "https://api.domainsdb.info/v1/domains/search?domain=facebook";
+  // apihref : string = "https://api.artic.edu/api/v1/artworks";
+  apihref : string = "https://gutendex.com/books/?languages=en";
 
-  apiResponse : Object;
+  apiResponse : any;
+
+  category = "";
+  subjects = "";
+  imgCover = "";
 
   constructor(appService: AppService) {
     this.appService = appService;
   }
 
-  getUrl(domain) {
+  ngOnInit(): void {
+    this.getUrl();
+  }
+
+  getUrl() {
     if (this.apihref != "") {
-      console.log( this.appService.getUrl(this.apihref) );
+      this.appService.getUrl(this.apihref).subscribe( (response) => {this.apiResponse = response} );
     }
   }
   
   refreshTable(value, table) {
-    this.getUrl("facebook");
     setTimeout( () => {
-      // for (let domain in this.apiResponse.domains.length)
-      console.log(this.apiResponse)
+      for (let i = 0; i < this.apiResponse.results.length; i++) {
+        let obj = this.apiResponse.results[i];
+        if (obj.title == value) {
+
+          console.log(obj)
+
+          this.category = "";
+          for (let c = 0; c < obj.bookshelves.length; c++) {
+            this.category += obj.bookshelves[c] + ", ";
+          }
+          
+          this.subjects = "";
+          for (let c = 0; c < obj.subjects.length; c++) {
+            this.subjects += obj.subjects[c] + ", ";
+          }
+          
+          this.imgCover = obj.formats['image/jpeg'];
+          
+          this.category = this.category || "Não informado"
+          this.subjects = this.subjects || "Não informado"
+          
+        }
+      }
+      
     }, 2000 )
   }
 
